88cf80568b534eca761c252d1b18807436bc4eff
enea-gjergo <55241618+enea-gjergo@users.noreply.github.com>
2019-09-13 10:13:58 +0200

    Update

--- gradmass.py
+++ gradmass.py
@@ -1,5 +1,5 @@
 #Gradmass zu Bogenmass
-gradmass0=float(input("Gradmass: "))
+bogenmass=float(input("Bogenmass: "))
 from math import pi
-bogenmass0=gradmass0*pi/180
-print(f"Bogenmass: {round(bogenmass0, 5)}rad")
+gradmasse=bogenmass*180/pi
+print(gradmasse,"°")
